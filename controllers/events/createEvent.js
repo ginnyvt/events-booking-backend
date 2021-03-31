5be0dde21375249108a4a8d0b5bccfefcfe89651
thqutran <ginny@powerkernel.com>
2021-03-31 15:06:32 +0300

    chore: checkout branch to update the remote repo

--- controllers/events/createEvent.js
+++ controllers/events/createEvent.js
@@ -1,5 +1,41 @@
 const createEvent_uc = require('../../usecase/events/createEvent');
 
-const invoke = (req) => {};
+const invoke = async (req) => {
+  if (!req.body.title) {
+    throw new Error('Title is required!');
+  }
+
+  if (!req.body.address) {
+    throw new Error('Address is required!');
+  }
+
+  if (!req.body.startTime) {
+    throw new Error('Starting time is required!');
+  }
+
+  if (!req.body.endTime) {
+    throw new Error('Ending time is required!');
+  }
+
+  if (!req.body.registerBefore) {
+    throw new Error('Registration before is required!');
+  }
+
+  if (!req.body.cancelBefore) {
+    throw new Error('Cancellation before is required!');
+  }
+
+  if (!req.body.maxParticipants) {
+    throw new Error('Max participants is required!');
+  }
+
+  if (!req.body.minParticipants) {
+    throw new Error('Min participants is required');
+  }
+
+  const 
+  const validatedEvent = req.body;
+  return await createEvent_uc.handle(validatedEvent);
+};
 
 module.exports = { invoke };
--- usecase/events/createEvent.js
+++ usecase/events/createEvent.js
@@ -1,3 +1,21 @@
-const handle = (validatedEvent) => {};
+const createEvent_uc = require('../../repositories/events');
+const Event = require('../../entities/Event');
+
+const handle = async (validatedEvent) => {
+  const {
+    title,
+    address,
+    startTime,
+    endTime,
+    registerBefore,
+    cancelBefore,
+    maxParticipants,
+    minParticipants,
+  } = validatedEvent;
+
+  const event = new Event();
+
+  return await createEvent_uc.insert(validatedEvent);
+};
 
 module.exports = { handle };
