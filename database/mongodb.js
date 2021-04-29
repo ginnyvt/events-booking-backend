3df963187e809ba51b9f537b6f54f8674bc1cedc
asus-ubuntu <ginny@powerkernel.com>
2021-04-29 11:01:20 +0300

    fix: connection to mongodb; do not connect if the connection has been already made

--- database/mongodb.js
+++ database/mongodb.js
@@ -2,21 +2,31 @@ const MongoClient = require('mongodb').MongoClient;
 
 const createHttpError = require('http-errors');
 
-const CONNECTION_URL = process.env.MONGO_URL;
+const url = process.env.MONGO_URL;
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const connectdb = async () => {
-  let database;
-  try {
-    const client = await MongoClient.connect(CONNECTION_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    database = await client.db('events-booking');
-    console.log('Connected to events-booking database');
-  } catch (err) {
-    throw createHttpError(500, err.message);
+  // let database;
+  // try {
+  //   const client = await MongoClient.connect(CONNECTION_URL, {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true,
+  //   });
+
+  //   database = await client.db('events-booking');
+  //   console.log('Connected to events-booking database');
+  // } catch (err) {
+  //   throw createHttpError(500, err.message);
+  // }
+  // return database;
+
+  if (!client.isConnected()) {
+    await client.connect();
   }
-  return database;
+  return client.db('events-booking');
 };
 
 module.exports = { connectdb };
